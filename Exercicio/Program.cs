0abe3df918860622d789d1f144b6df37fb7a0441
claudiasaito <claudia.saito@outlook.com>
2020-04-03 11:06:01 -0300

    Add project files.

---
+++ Exercicio/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario, vale;
+
+            Console.WriteLine("Digite o valor do seu salário");
+            try
+            {
+                salario = Convert.ToDouble(Console.ReadLine());
+            }
+            catch(Exception err)
+            {
+                //throw err;
+                Console.WriteLine("Erro no sistema, digite um número");
+                return;
+            }
+            Console.WriteLine("Digite o valor do seu vale");
+            try
+            {
+                vale = Convert.ToDouble(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Erro no sistema, digite um número");
+                return;
+            }
+
+            double total = salario + vale;
+
+            Console.WriteLine("O valor total é " + total);
+        }
+
+        
+    }
+}
---
+++ LogicaAvanade/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LogicaAvanade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Digite o primeiro número da operação");
+            try
+            {
+                a = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Erro no sistema, você precisa digitar um número");
+                return;
+            }
+            
+            Console.WriteLine("Digite o segundo número da operação");
+            try
+            {
+                b = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Erro no sistema, você precisa digitar um número");
+                return;
+            }
+            int c = a + b;
+
+            Console.WriteLine("O resultado da operação é:" + c);
+            
+            Console.ReadLine();
+        }
+    }
+}
